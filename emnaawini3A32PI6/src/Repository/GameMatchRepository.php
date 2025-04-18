d6efc24c33aa7868f97d0e7e968d2160f00675ed
emma awini <94049201+emmaeryne@users.noreply.github.com>
2025-04-18 11:01:02 +0100

    uu

---
+++ emnaawini3A32PI6/src/Repository/GameMatchRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\GameMatch;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\GameMatchRepository;
+
+/**
+ * @extends ServiceEntityRepository<GameMatch>
+ */
+class GameMatchRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, GameMatch::class);
+    }
+}
