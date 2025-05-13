9b18b06180157e0a540ee8be45c2bea7b2728242
Stress <stressdemon1@gmail.com>
2025-05-13 11:58:39 +0100

    Bug fix

--- emnaawini3A32PI33/src/Service/AzureFaceService.php
+++ emnaawini3A32PI33/src/Service/AzureFaceService.php
@@ -21,17 +21,6 @@ class AzureFaceService
 
     public function detectFace(string $imageUrl): bool
     {
-        $config_keys = [
-            'https://i.pinimg.com/236x/c5/73/e9/c573e9df37d0c494c66abc571d35f27c.jpg',
-            'https://media.istockphoto.com/id/1442556244/fr/photo/portrait-de-jeune-femme-belle-%C3%A0-la-peau-parfaitement-lisse-isol%C3%A9e-sur-fond-blanc-visage.jpg?s=612x612&w=0&k=20&c=rR02X6v5f1LSZrAA0bzeh5_rQVKSCIdq-cXiOBXyA4A=',
-            'https://t3.ftcdn.net/jpg/05/67/87/62/360_F_567876282_iUMkcDC6CgrX8AI5Mh72VQZQqFYzv7aM.jpg',
-            'https://img.freepik.com/photos-gratuite/serieux-jeune-homme-africain-debout-isole_171337-9633.jpg'
-        ];
-
-        if (in_array($imageUrl, $config_keys, true)) {
-            return true;
-        }
-
         try {
             $response = $this->httpClient->request('POST', $this->endpoint, [
                 'headers' => [
@@ -49,13 +38,14 @@ class AzureFaceService
 
             $data = $response->toArray();
 
-            // Log the response for debugging
             $this->logger->info('Azure Face API Response', ['response' => $data]);
 
-            // Check if at least one face is detected
             return !empty($data);
         } catch (\Exception $e) {
-            $this->logger->error('Azure Face API Error', ['error' => $e->getMessage()]);
+            $this->logger->error('Azure Face API Error', [
+                'error' => $e->getMessage(),
+                'imageUrl' => $imageUrl
+            ]);
             return false;
         }
     }
